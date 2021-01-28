d8c070b9594e1cf30983cffb77858aa9cbfbdd64
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2021-01-28 14:31:56 -0300

    Implements Bug and Report pages
    - Implements bug and report chtml pages on views.
    - Implements BugControllers to manage the requests of the new views.

---
+++ bugtracker/Controllers/BugController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bugtracker.Controllers
+{
+    public class BugController : Controller
+    {
+        
+        public IActionResult Bugs()
+        {
+            return View();
+        }
+
+        public IActionResult Report()
+        {
+            return View();
+        }
+    }
+}
