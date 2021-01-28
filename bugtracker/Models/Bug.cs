1b9d3d2d1cf27be81d8024290cdedd3219c981b9
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2021-01-28 09:50:33 -0300

    Implements model bug

---
+++ bugtracker/Models/Bug.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bugtracker.Models
+{
+    public class Bug
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Product { get; set; }
+        public string Version { get; set; }
+        public string Status { get; set; }
+        public string Type { get; set; } // Security, Usability etc.
+    }
+}
