9345797fceb831beeb045cbd24ac09545f588805
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2021-02-03 12:44:51 -0300

    Add Display-Name property for each classe item

--- bugtracker/Models/Bug.cs
+++ bugtracker/Models/Bug.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +10,17 @@ namespace bugtracker.Models
     public class Bug
     {
         public int Id { get; set; }
+        [Display(Name = "Titulo")]
         public string Title { get; set; }
+        [Display(Name = "Descrição")]
         public string Description { get; set; }
+        [Display(Name = "Produto")]
         public string Product { get; set; }
+        [Display(Name = "Versão")]
         public string Version { get; set; }
+        [Display(Name = "Status")]
         public string Status { get; set; }
+        [Display(Name = "Tipo")]
         public string Type { get; set; } // Security, Usability etc.
     }
 }
