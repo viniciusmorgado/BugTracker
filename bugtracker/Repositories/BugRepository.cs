6508817333443feac695fd56998f9bbadc8a7d9e
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2021-03-06 12:50:00 -0300

    Implements Repositories layer
    - Add new repositories layer to the project directory.
    - Auto-update of launch settings properties.

---
+++ bugtracker/Repositories/BugRepository.cs
@@ -0,0 +1,24 @@
+﻿using bugtracker.Data;
+using bugtracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bugtracker.Repositories
+{
+    public class BugRepository : IBugRepository
+    {
+        private readonly BugDbContext _bugDbContext;
+
+        public BugRepository(BugDbContext bugDbContext)
+        {
+            _bugDbContext = bugDbContext;
+        }
+
+        public void Save(Bug bug)
+        {
+            _bugDbContext.Bugs.Add(bug);
+        }
+    }
+}
---
+++ bugtracker/Repositories/IBugRepository.cs
@@ -0,0 +1,13 @@
+﻿using bugtracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bugtracker.Repositories
+{
+    public interface IBugRepository
+    {
+        void Save(Bug bug);
+    }
+}
