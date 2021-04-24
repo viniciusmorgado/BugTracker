d6f7c9a6f7661c2bba7a5a8fe7f1a5cbabd12e54
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2021-04-24 13:07:21 -0300

    Add Identity Scaffolding

---
+++ bugtracker/Areas/Identity/Data/bugtrackerUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace bugtracker.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the bugtrackerUser class
+    public class bugtrackerUser : IdentityUser
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ bugtracker/Data/Config/OAuthUserConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using bugtracker.Areas.Identity.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bugtracker.Data.Config
+{
+    public class OAuthUserConfiguration : IEntityTypeConfiguration<bugtrackerUser>
+    {
+        public void Configure(EntityTypeBuilder<bugtrackerUser> builder)
+        {
+            builder.Property(prop => prop.Email).IsRequired();
+            builder.Property(prop => prop.Password).IsRequired();
+
+        }
+    }
+}
